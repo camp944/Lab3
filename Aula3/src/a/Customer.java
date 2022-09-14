3aea01c8b731f9bf0dab979acae1aac66006eb45
João Pedro de Almeida Campos <camp944@hotmail.com>
2022-09-14 11:30:57 -0300

    Commit 4

--- Aula3/src/a/Customer.java
+++ Aula3/src/a/Customer.java
@@ -30,7 +30,7 @@ public class Customer {
 
         //determine amounts for each line
         
-        thisAmount = amountFor(each);
+        thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -51,23 +51,5 @@ public class Customer {
      return result;
    }
   
-  private double amountFor(Rental aRental) {
-	  double thisAmount = 0;
-	  switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         thisAmount += 2;
-         if (aRental.getDaysRented() > 2)
-            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         thisAmount += aRental.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         thisAmount += 1.5;
-         if (aRental.getDaysRented() > 3)
-            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-          break;
-	  }
-	  return thisAmount;
-   }
+  
 }
