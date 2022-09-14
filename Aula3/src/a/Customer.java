88a6ce136757e4e1135606dd9c8054e65e43ae73
João Pedro de Almeida Campos <camp944@hotmail.com>
2022-09-14 11:33:16 -0300

    Commit 5

--- Aula3/src/a/Customer.java
+++ Aula3/src/a/Customer.java
@@ -25,12 +25,8 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
         
-        thisAmount = each.getCharge();
+        Rental each = (Rental) rentals.nextElement();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -40,8 +36,8 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
      //add footer lines
